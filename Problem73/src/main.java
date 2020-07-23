2ccce04578c764ae087a88993a9b663090736adc
Eli <eestridge@miis.edu>
2020-07-23 13:51:15 -0400

    Adding files into their own directory and adding gitignore.

---
+++ Problem73/src/main.java
@@ -0,0 +1,27 @@
+/**
+ * @author Eli Estridge; eestridge@middlebury.edu
+ * A visualization of problem 73, "Rooster Chase," in Levitin & Levitin's Algorithmic Puzzles.
+ */
+
+
+public class main {
+	/* Initializes game board using the parameters Gamespace(boardsize, isRoostersTurn)
+	 * set isRoostersTurn false to make the farmer go first.  Set the board size to 8 for the board size of the
+	 * original problem (it shouldn't matter since it is a problem of parity). */
+	public static void main(String[] args) {
+		Gamespace game = new Gamespace(8, false);
+		game.drawGame();
+		while (game.isCaught() == false) {
+			
+			//Adjust for better experience.  sleep(n) = n milliseconds.  
+			try { Thread.sleep(500);} 
+			catch (InterruptedException e) { Thread.currentThread().interrupt();e.printStackTrace();}
+			
+			game.move();
+			game.drawGame();
+
+		}
+		System.out.println("CAUGHT!!!");
+	}
+
+}
